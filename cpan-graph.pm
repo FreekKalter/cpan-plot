f5b0dedca8e9facf9d7def93d8d173fb90d6408c
FreekKalter <freek@kalteronline.org>
2012-06-17 12:09:02 +0200

    first

---
+++ cpan-graph.pm
@@ -0,0 +1,60 @@
+package CPAN::TesterStats::Get;
+
+use v5.12;
+use strict;
+use warnings;
+
+use utf8;
+
+use LWP; 
+use HTML::TreeBuilder;
+use Data::Dumper qw( Dumper );
+use DateTime;
+
+my %scores = (
+    total   => { address => "http://stats.cpantesters.org/testers.html" ,
+                 tests => 0 },
+    mac     => { address => "http://stats.cpantesters.org/leaders/leaders-darwin-this.html" ,
+                 tests => 0 } ,
+    win32   => { address => "http://stats.cpantesters.org/leaders/leaders-mswin32-this.html" , 
+                 tests => 0 },
+);
+my $now = DateTime->now( time_zone => 'local' );
+
+for my $key (keys %scores){
+
+    my $web  = do_GET($scores{$key}{address});
+    my $tree = HTML::TreeBuilder->new;
+    $tree->parse($web);
+
+    my $tr = $tree->look_down(
+        '_tag' => 'tr',
+        sub { $_[0]->look_down(
+                        '_tag' => 'td',
+                        sub{ $_[0]->as_text =~ m/freek.*kalteronline.*org/ }
+            )},
+    );                   
+    my @trs = $tr->descendants(); 
+    open my $dh, ">>" , "${key}.data";
+    print $dh $now->ymd, "\t";
+    if($key eq 'total'){
+        print $dh $trs[1]->as_text(), "\n";
+        $scores{$key}{tests} = $trs[1]->as_text();
+    }else{
+        print $dh $trs[2]->as_text(), "\n";
+        $scores{$key}{tests} = $trs[2]->as_text();
+    }
+}
+
+say Dumper(%scores);
+
+
+sub do_GET {
+    my $browser;
+    $browser = LWP::UserAgent->new unless $browser;
+    my $resp = $browser->get(@_);
+    return ( $resp->content, $resp->status_line, $resp->is_success, $resp )
+      if wantarray;
+    return unless $resp->is_success;
+    return $resp->content;
+}
---
+++ plot.pl
@@ -0,0 +1,31 @@
+package CPAN::TesterStats::Plot;
+use v5.12;
+use strict;
+use warnings;
+
+use utf8;
+
+use Chart::Gnuplot;
+
+# Data
+my @x = (-10 .. 10);
+my @y = (0 .. 20);
+
+# Create chart object and specify the properties of the chart
+my $chart = Chart::Gnuplot->new(
+    output => "fig/simple.png",
+    title  => "Simple testing",
+    xlabel => "My x-axis label",
+    ylabel => "My y-axis label",
+);
+
+# Create dataset object and specify the properties of the dataset
+my $dataSet = Chart::Gnuplot::DataSet->new(
+    xdata => \@x,
+    ydata => \@y,
+    title => "Plotting a line from Perl arrays",
+    style => "linespoints",
+);
+
+## Plot the data set on the chart
+$chart->plot2d($dataSet);
