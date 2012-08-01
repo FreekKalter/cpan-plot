d60910c7715edc88336a4bc70ce597e83337e263
Freek Kalter <freek@kalteronline.org>
2012-08-01 19:20:11 +0200

    Added legend, give datafile location on cmdline

--- plot.pl
+++ plot.pl
@@ -9,9 +9,10 @@ use Cwd;
 use Chart::Gnuplot;
 use Path::Class;
 
+my $dir = $ARGV[0] || ".";
 my ( @sets, @delta_sets );
-for my $file ( qw( total win32 mac ) ){
-    open my $fh , "<" , "$file.data" or die "Could not open $file: $!";
+for my $file ( qw( total win32 mac linux) ){
+    open my $fh , "<" , file($dir, "$file.data") or die "Could not open $file: $!";
     my ( @x, @y, @x_delta, @y_delta );
 
     <$fh> =~ /(\d{4}-\d{2}-\d{2})\t(\d*)/ ; 
@@ -29,26 +30,36 @@ for my $file ( qw( total win32 mac ) ){
     }
 
     push @sets , Chart::Gnuplot::DataSet->new(
-        xdata  => \@x,
-        ydata  => \@y,
-        style  => 'linespoints',
+        title   => "$file",
+        xdata   => \@x,
+        ydata   => \@y,
+        style   => 'linespoints',
         timefmt => '%Y-%m-%d',      # input time format
     );
     
     push @delta_sets , Chart::Gnuplot::DataSet->new(
-        xdata  => \@x_delta,
-        ydata  => \@y_delta,
-        style  => 'linespoints',
+        title   => "$file",
+        xdata   => \@x_delta,
+        ydata   => \@y_delta,
+        style   => 'linespoints',
         timefmt => '%Y-%m-%d',      # input time format
     );
 }
 
 # Initiate the chart object
-my $output = file(getcwd(), "output.png");
+my $output = file($dir, "output.png");
 my $chart = Chart::Gnuplot->new(
    output   => $output->as_foreign('Unix'),
    xlabel   => 'Date axis',
    ylabel   => 'Number of test on cpan',
+   bg       => 'white',
+   legend   => {
+        position    => "outside bottom",
+        width       => 3,
+        height      => 4,
+        align       => "right",
+        title       => "Legend",
+   },
    timeaxis => "x",
    xtics    => {
         labelfmt => '%y/%m/%d',   
@@ -61,11 +72,19 @@ $chart->gnuplot('wgnuplot.exe') if ($^O eq 'MSWin32');
 $chart->plot2d(@sets);
 
 # Initiate the chart object
-$output = file(getcwd(), "delta_output.png");
+$output = file($dir, "delta_output.png");
 $chart = Chart::Gnuplot->new(
    output   => $output->as_foreign('Unix'),
    xlabel   => 'Date axis',
    ylabel   => 'Number of test per day',
+   bg       => 'white',
+   legend   => {
+        position    => "outside bottom",
+        width       => 3,
+        height      => 4,
+        align       => "right",
+        title       => "Legend",
+   },
    timeaxis => "x",
    xtics    => {
         labelfmt => '%y/%m/%d',   
