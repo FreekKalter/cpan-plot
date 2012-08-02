6a9642deb8018d0cb1c984144b4ce5e41b8d19db
Freek Kalter <freek@kalteronline.org>
2012-08-02 13:42:49 +0200

    dont run cpan-extracter if already run today

--- cpan-extracter.pl
+++ cpan-extracter.pl
@@ -3,13 +3,14 @@ package CPAN::TesterStats::Get;
 use v5.12;
 use strict;
 use warnings;
-
 use utf8;
 
 use LWP; 
 use HTML::TreeBuilder;
 use Data::Dumper qw( Dumper );
 use DateTime;
+use File::ReadBackwards;
+use Path::Class;
 
 my $dir = $ARGV[0] || ".";
 my %scores = (
@@ -32,24 +33,38 @@ my %scores = (
 );
 my $now = DateTime->now( time_zone => 'local' );
 for my $key (keys %scores){
+    #check last date
+    my $last_date;
+    my $filename = file($dir , "${key}.data");
+    if(-e $filename ){ 
+        my $bw = File::ReadBackwards->new( $filename ) or die "Could not read ${key}.data backwards: $!";
+        my $last_line = $bw->readline;
+        chomp $last_line;
+        $bw->close;
+        $last_line =~ /( \d{4} - \d{2} - \d{2} )  \t (\d*)/x;
+        $last_date = $1;
+    }else{
+        $last_date = 0;
+    }
+    
+    if ($now->ymd ne $last_date){
+        my $web  = do_GET($scores{$key}{address});
+        my $tree = HTML::TreeBuilder->new;
+        $tree->parse($web);
 
-    my $web  = do_GET($scores{$key}{address});
-    my $tree = HTML::TreeBuilder->new;
-    $tree->parse($web);
+        my $tr = $tree->look_down(
+            '_tag' => 'tr',
+            sub { $_[0]->look_down(
+                            '_tag' => 'td',
+                            sub{ $_[0]->as_text =~ m/kalter/i }
+                )},
+        );                   
+        my @trs = $tr->descendants(); 
 
-    my $tr = $tree->look_down(
-        '_tag' => 'tr',
-        sub { $_[0]->look_down(
-                        '_tag' => 'td',
-                        sub{ $_[0]->as_text =~ m/kalter/i }
-            )},
-    );                   
-    my @trs = $tr->descendants(); 
-
-    open my $dh, ">>" , "$dir/${key}.data";
-    print $dh $now->ymd, "\t";
-    say $dh $trs[$scores{$key}{column}]->as_text();
-    close $dh;
+        open my $dh, ">>" , $filename;
+        print $dh $now->ymd, "\t" , $trs[$scores{$key}{column}]->as_text(), "\n";
+        close $dh;
+    }
 }
 
 sub do_GET {
