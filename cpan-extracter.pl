d5fbb3db31542ff236a8b23e54241390fa77d408
Freek Kalter <freek@kalteronline.org>
2012-08-06 20:23:31 +0200

    fixed bug

--- cpan-extracter.pl
+++ cpan-extracter.pl
@@ -34,45 +34,41 @@ my %scores = (
 my $now = DateTime->now( time_zone => 'local' );
 for my $key (keys %scores){
     #check last date
-    my $last_date;
+    my $last_date = 0;
     my $filename = file($dir , "${key}.data");
     if(-e $filename ){ 
-        my $bw = File::ReadBackwards->new( $filename ) or die "Could not read ${key}.data backwards: $!";
+        my $bw = File::ReadBackwards->new( $filename ) or die "Could not read $filename backwards: $!";
         my $last_line = $bw->readline;
         chomp $last_line;
         $bw->close;
         $last_line =~ /( \d{4} - \d{2} - \d{2} )  \t (\d*)/x;
         $last_date = $1;
-    }else{
-        $last_date = 0;
     }
     
-    if ($now->ymd ne $last_date){
-        my $web  = do_GET($scores{$key}{address});
-        my $tree = HTML::TreeBuilder->new;
-        $tree->parse($web);
+    next if ($now->ymd eq $last_date);
+    my $web  = do_GET($scores{$key}{address});
+    my $tree = HTML::TreeBuilder->new;
+    $tree->parse($web);
 
-        my $tr = $tree->look_down(
-            '_tag' => 'tr',
-            sub { $_[0]->look_down(
-                            '_tag' => 'td',
-                            sub{ $_[0]->as_text =~ m/kalter/i }
-                )},
-        );                   
-        my @trs = $tr->descendants(); 
+    my $tr = $tree->look_down(
+        '_tag' => 'tr',
+        sub { $_[0]->look_down(
+                    '_tag' => 'td',
+                    sub{ $_[0]->as_text =~ m/kalter/i }
+        )},
+    );                   
+    my @trs = $tr->descendants(); 
 
-        open my $dh, ">>" , $filename;
-        print $dh $now->ymd, "\t" , $trs[$scores{$key}{column}]->as_text(), "\n";
-        close $dh;
-    }
+    open my $dh, ">>" , $filename;
+    print $dh $now->ymd, "\t" , $trs[$scores{$key}{column}]->as_text(), "\n";
+    close $dh;
 }
 
 sub do_GET {
     my $browser;
     $browser = LWP::UserAgent->new unless $browser;
     my $resp = $browser->get(@_);
-    return ( $resp->content, $resp->status_line, $resp->is_success, $resp )
-      if wantarray;
+    return ( $resp->content, $resp->status_line, $resp->is_success, $resp ) if wantarray;
     return unless $resp->is_success;
     return $resp->content;
 }
